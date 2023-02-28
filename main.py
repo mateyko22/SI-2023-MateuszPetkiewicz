74c875e33271ae8b70dff37cedf3202aa95cdee3
mateyko22 <petmat@o2.pl>
2023-02-28 18:58:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+diabetes = np.loadtxt('dane/diabetes.txt', dtype="str")
+diabetes_type = np.loadtxt('dane/diabetes_type.txt', dtype="str")
+info = np.loadtxt('dane/_info-data-discrete.txt')
