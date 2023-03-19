490874196140e797f25b71a9614f48c6e4970061
mateyko22 <petmat@o2.pl>
2023-03-19 17:51:03 +0100

    cw2

--- main.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-diabetes = np.loadtxt('dane/diabetes.txt', dtype="str")
-diabetes_type = np.loadtxt('dane/diabetes_type.txt', dtype="str")
-info = np.loadtxt('dane/_info-data-discrete.txt')
